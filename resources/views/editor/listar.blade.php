51d5876281d784371db6cab4a9b34c6b22134be7
Juan C. Cruz <juancruz@udenar.edu.co>
2021-02-06 22:21:25 -0500

    Modificación Final

--- resources/views/editor/listar.blade.php
+++ resources/views/editor/listar.blade.php
@@ -1,34 +1,34 @@
 @extends('layout.master')
     @section('content')
+ <body style="background-color:#6c757d"> 
+        <br>
+        <h2><font color="black"><center>Listado de Editorial </center></font></h2>
 
-    <br>
-    <h2><font color="black"><center>Listado de Editorial </center></font></h2>
-
-    <br>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Dirección</th>
-                <th scope="col">Ciudad</th>
-                <th scope="col">Telefono</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($editorial as $e )
+        <br>
+        <table class="table" border=4 style="border-color:#000000;">
+            <thead>
                 <tr>
-                    <td>{{$e->id}}</td>
-                    <td>{{$e->nombre}}</td>
-                    <td>{{$e->direccion}}</td>
-                    <td>{{$e->ciudad}}</td>
-                    <td>{{$e->telefono}}</td>
-                    
-                    <td><a style="color:#000000; background-color:#5592d4; border-color:#000000;" href="{{route('formulario_edi', $e->id)}}" class="btn btn-success"> Editar </a></td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Dirección</th>
+                    <th scope="col">Ciudad</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col"></th>
                 </tr>
-            @endforeach 
-        </tbody>
-    </table> 
-
+            </thead>
+            <tbody>
+                @foreach($editorial as $e )
+                    <tr>
+                        <td>{{$e->id}}</td>
+                        <td>{{$e->nombre}}</td>
+                        <td>{{$e->direccion}}</td>
+                        <td>{{$e->ciudad}}</td>
+                        <td>{{$e->telefono}}</td>
+                        
+                        <td><a style="color:#000000; background-color:#5592d4; border-color:#000000;" href="{{route('formulario_edi', $e->id)}}" class="btn btn-success"> Editar </a></td>
+                    </tr>
+                @endforeach 
+            </tbody>
+        </table> 
+  </body>
 @stop
--- resources/views/libros/listlibros.blade.php
+++ resources/views/libros/listlibros.blade.php
@@ -1,40 +1,41 @@
 @extends('layout.master')
     @section('content')
+    <body style="background-color:#6c757d">
+        <br>
+        <h2><font color="black"><center>Listado de Libros </center></font></h2>
 
-    <br>
-    <h2><font color="black"><center>Listado de Libros </center></font></h2>
-
-    <br>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">ISBN</th>
-                <th scope="col">Titulo</th>
-                <th scope="col">Precio</th>
-                <th scope="col">Editorial</th>
-                <th scope="col">Descuento</th>
-                
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($libro as $lib )
+        <br>
+        <table class="table" border=4 style="border-color:#000000;">
+            <thead>
                 <tr>
-                    <td>{{$lib->id}}</td>
-                    <td>{{$lib->ISBN}}</td>
-                    <td>{{$lib->titulo}}</td>
-                    <td>{{$lib->precio}}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Editorial</th>
+                    <th scope="col">Descuento</th>
                     
-
-                    @foreach($editorial as $edi)
-                        @if($lib->editorial_fk == $edi->id)
-                            <td>{{$edi->nombre}}</td>
-                        @endif 
-                    @endforeach 
-                    <td></td>
                 </tr>
-            @endforeach 
-        </tbody>
-    </table> 
+            </thead>
+            <tbody>
+                @foreach($libro as $lib )
+                    <tr>
+                        <td>{{$lib->id}}</td>
+                        <td>{{$lib->ISBN}}</td>
+                        <td>{{$lib->titulo}}</td>
+                        <td>{{$lib->precio}}</td>
+                        
+
+                        @foreach($editorial as $edi)
+                            @if($lib->editorial_fk == $edi->id)
+                                <td>{{$edi->nombre}}</td>
+                            @endif 
+                        @endforeach 
+                        <td></td>
+                    </tr>
+                @endforeach 
+            </tbody>
+        </table> 
+    </body>    
 
 @stop
